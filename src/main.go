de2a21d9b927634248c6ccef1973d1bc592adc5d
Leonardo Costa <lbcosta.dev@gmail.com>
2026-02-04 20:24:48 -0300

    Add Gmail integration and update dependencies

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"inky/src/modules/db"
+	"inky/src/modules/gmail"
 	"inky/src/modules/printer"
 	"net/http"
 )
@@ -16,6 +17,17 @@ func main() {
 					w.Write([]byte(err.Error()))
 				}
 				printer.Print("Inky says: " + randomString)
+				gmail.PrintUserLabels()
+				w.WriteHeader(200)
+				w.Write([]byte("Printed successfully"))
+			}
+		})
+
+	http.DefaultServeMux.
+		HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodGet {
+				w.WriteHeader(200)
+				w.Write([]byte("You can now return to the application."))
 			}
 		})
 
